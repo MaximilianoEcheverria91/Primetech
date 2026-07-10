242d05ed399e5c9268117ca00ba55e258db0ca7d
MaximilianoEcheverria91 <maximilianoEcheverria91@gmail.com>
2026-07-10 18:23:07 -0300

    feat: inicio de proyecto Primetech

---
+++ backend/src/test/java/com/primetech/PrimetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
